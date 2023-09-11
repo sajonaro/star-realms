85bb0f794d52fe7270c6be1cfc890c82808bbfb1
sajonaro <alex.kunich@gmail.com>
2023-09-11 20:04:42 +0300

    updated

--- project.clj
+++ project.clj
@@ -1,12 +1,20 @@
-(defproject game "0.1.0-SNAPSHOT"
+(defproject game "starrealms"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler game.handler/app}
+                 [compojure/compojure "1.6.2"]
+                 [ring/ring-defaults "0.3.3"]
+                 [ring/ring-jetty-adapter "1.9.4"]
+                 [hiccup/hiccup "2.0.0-alpha2"]
+                 [metosin/muuntaja "0.6.8"]]
+  
+  
+  :plugins [[lein-ring "0.12.6"]]
+  :ring {:handler game.handler/app
+         :port 3000} 
+  :uberjar-name "starrealms.jar"
+  :main game.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
--- src/game/handler.clj
+++ src/game/handler.clj
@@ -1,13 +1,31 @@
 (ns game.handler
-  (:require [compojure.core :refer :all]
+  (:gen-class)
+  (:require [compojure.core :as c]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults api-defaults]]))
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :as ring-defaults]
+            [muuntaja.middleware :as muuntaja]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+(defonce server (atom nil))
+
+(c/defroutes app-routes
+  (c/GET "/" [] "Hello World")
   (route/resources "/")
   (route/not-found "Not Found"))
 
-
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (ring-defaults/wrap-defaults app-routes ring-defaults/api-defaults))
+
+(defn handler [req]
+  ((app-routes) req))
+
+(defn start-jetty! []
+  (reset! server
+   (jetty/run-jetty (-> #'handler
+                        muuntaja/wrap-format
+                        (ring-defaults/wrap-defaults ring-defaults/api-defaults))
+                    {:join? false
+                     :port 3000})))
+
+(defn -main [& args]
+  (start-jetty!))
